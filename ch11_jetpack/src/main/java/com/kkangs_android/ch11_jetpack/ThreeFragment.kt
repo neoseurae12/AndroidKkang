b4ab77644096443a0fbbe7dae527da5dd1b71131
Wonhui Roh <rwhui012@gmail.com>
2023-07-29 00:35:26 +0900

    Ch.11_제트팩 라이브러리_제트팩 화면

---
+++ ch11_jetpack/src/main/java/com/kkangs_android/ch11_jetpack/ThreeFragment.kt
@@ -0,0 +1,19 @@
+package com.kkangs_android.ch11_jetpack
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kkangs_android.ch11_jetpack.databinding.FragmentThreeBinding
+
+class ThreeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return return FragmentThreeBinding.inflate(inflater, container, false).root
+    }
+}
